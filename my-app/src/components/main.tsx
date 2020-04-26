96e2a17ddc224f0c819c92d79e38cb6740d11ddf
taehoonkwon <kwonth211@gmail.com>
2020-04-26 21:45:40 +0900

    commit

--- my-app/src/components/main.tsx
+++ my-app/src/components/main.tsx
@@ -1,22 +1,42 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+
+
 type GreetingsProps = {
   name: string;
   mark: string;
 };
 
 
+
+
 function Greetings({ name, mark }: GreetingsProps) {
   return (
-    <div>
 
-    <Button variant="contained" color="primary">
-      click me
-    </Button>
 
     <div>
-       {name} {mark}
-    </div>
+      <AppBar position="static">
+        {/* <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+    </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar> */}
+      </AppBar>
+      <div>
+
+        <Button variant="contained" color="primary">
+          click me
+      </Button>
+
+        <div>
+          {name} {mark}
+        </div>
+      </div>
     </div>
   );
 }
