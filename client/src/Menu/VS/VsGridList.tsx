0b4a2a4aeaa15590173a15877b58aa76f8d57f96
taehoonkwon <kwonth211@gmail.com>
2020-06-08 22:36:42 +0900

    이미지 너무적다..

--- client/src/Menu/VS/VsGridList.tsx
+++ client/src/Menu/VS/VsGridList.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react"
-import Button from "@material-ui/core/Button"
-import Card from "@material-ui/core/Card"
-import CardMedia from "@material-ui/core/CardMedia"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import Grid from "@material-ui/core/Grid"
-import Typography from "@material-ui/core/Typography"
-import { makeStyles } from "@material-ui/core/styles"
-import Container from "@material-ui/core/Container"
-import Link from "@material-ui/core/Link"
-import { imageArr } from "./imageArr"
-import "./../../App.css"
-import { gql } from "apollo-boost"
-import { useQuery, useLazyQuery } from "@apollo/react-hooks"
+import React, { useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import { imageArr } from "./imageArr";
+import "./../../App.css";
+import { gql } from "apollo-boost";
+import { useQuery, useLazyQuery } from "@apollo/react-hooks";
 
 function Copyright() {
   return (
@@ -23,7 +23,7 @@ function Copyright() {
       {new Date().getFullYear()}
       {"."}
     </Typography>
-  )
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -56,29 +56,26 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
-}))
+}));
 
 // const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const cards = imageArr
+const cards = imageArr;
 
 interface Movies {
-  imgUrl: String
-  cast: [String]
-  runtime: String
-  released: [String]
-  directors: [String]
-  writers: [String]
-  year: String
-  countries: [String]
-  languages: [String]
-  profit: String
-  name: String
-  votes: Number
-  hashTag: [String]
-  genre: [String]
-}
-interface getMovieGenre {
-  genreMoves: Movies[]
+  imgUrl: string;
+  cast: [string];
+  runtime: string;
+  released: [string];
+  directors: [string];
+  writers: [string];
+  year: string;
+  countries: [string];
+  languages: [string];
+  profit: string;
+  name: string;
+  votes: Number;
+  hashTag: [string];
+  genre: [string];
 }
 
 const GETMOVIEGENRE = gql`
@@ -101,22 +98,35 @@ const GETMOVIEGENRE = gql`
       genre
     }
   }
-`
+`;
 const VsGridList: React.FunctionComponent<{ genre: String }> = ({ genre }) => {
-  const classes = useStyles()
-  // const [getMovieGenre] = useLazyQuery<[getMovieGenre, { genre: String }]>(GETMOVIEGENRE, {
-  //   variables: { genre: String },
-  // })
-  const [getMovieGenre, { called, loading, data }] = useLazyQuery(GETMOVIEGENRE)
-  // const [getMovieGenre, []] = useQuery(GETMOVIEGENRE)
+  const classes = useStyles();
+  const [getMovieGenre, { called, loading, data }] = useLazyQuery(
+    GETMOVIEGENRE
+  );
+  const [imageArr, setImageArr] = useState<Movies[]>([]);
 
+  const totalImage = [[], [], [], []];
   useEffect(() => {
-    // getMovieGenre(genre)
-    // let a = getMovieGenre()
-    // console.log(a)
-    let genreArr = getMovieGenre({ genre })
-    // console.log(genreArr)
-  }, [genre])
+    getMovieGenre({ variables: { genre } });
+    let imageList = data?.getMovieGenre;
+
+    // 이차원 배열로 16개씩 4개 담는다. 64개..
+    if (imageList) {
+      imageList = imageList.filter((iter) => {
+        if (iter.imgUrl) {
+          return iter;
+        }
+      });
+      // 처음엔 totalImage 중 0번째 .. 그이후에는 [1] [2][3] 식으로..
+      setImageArr(imageList);
+    }
+  }, [data]);
+
+  useEffect(() => {});
+
+  if (called && loading) return <p>Loading ...</p>;
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -126,10 +136,21 @@ const VsGridList: React.FunctionComponent<{ genre: String }> = ({ genre }) => {
 
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <Typography component="h5" variant="h5" align="center" color="textPrimary" gutterBottom>
+            <Typography
+              component="h5"
+              variant="h5"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               kwonth211 님 , 좋아하는 영화 총 16개를 PICK 해주세요
             </Typography>
-            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+            <Typography
+              variant="h6"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
               총 1개의 영화가 투표권수 1개 입니다.
             </Typography>
             <div className={classes.heroButtons}>
@@ -151,24 +172,20 @@ const VsGridList: React.FunctionComponent<{ genre: String }> = ({ genre }) => {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={3}>
-            {cards.map((card) => (
-              <Grid item key={card} sm={2} md={2}>
+            {imageArr.map((iterImage, i) => (
+              <Grid item key={i} sm={2} md={2}>
                 <Card
                   style={{ height: "175px", width: "125px" }}
                   className={classes.card}
-                  // className={"tracking-in-contract-bck"}
                 >
                   <CardMedia
                     className={"tracking-in-contract-bck"}
-                    // className={classes.cardMedia}
                     style={{
                       width: "100%",
                       height: "100%",
-                      // maxWidth: "100%",
-                      // maxHeight: "100%",
                     }}
-                    image={card}
-                    title="Image title"
+                    image={"https://" + iterImage.imgUrl}
+                    title={iterImage.name}
                   >
                     <div className="darkness"></div>
                     <div className="btn-plus">
@@ -187,7 +204,7 @@ const VsGridList: React.FunctionComponent<{ genre: String }> = ({ genre }) => {
       </footer>
       {/* End footer */}
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default VsGridList
+export default VsGridList;
