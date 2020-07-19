aaf843d382c1177af1eecf98c8e8f391bf4aa3dc
taehoonkwon <kwonth211@gmail.com>
2020-07-19 21:21:44 +0900

    깊은복사의 중요성 commit

--- client/src/views/Menu/VS/components/MymovieDialog.tsx
+++ client/src/views/Menu/VS/components/MymovieDialog.tsx
@@ -1,59 +1,69 @@
-import React, { useCallback, useState, useEffect } from "react"
-import Button from "@material-ui/core/Button"
-import Dialog from "@material-ui/core/Dialog"
-import DialogActions from "@material-ui/core/DialogActions"
-import DialogContent from "@material-ui/core/DialogContent"
-import DialogContentText from "@material-ui/core/DialogContentText"
-import DialogTitle from "@material-ui/core/DialogTitle"
-import useMediaQuery from "@material-ui/core/useMediaQuery"
-import { makeStyles, createStyles, useTheme, Theme } from "@material-ui/core/styles"
-import { SearchBox } from "../../../Main/SearhMain/components/SearchBox"
-import { green, red } from "@material-ui/core/colors"
-import Icon from "@material-ui/core/Icon"
-import { useStyles } from "./styles"
-import MovieList from "./SearchMovieList/SearchMovieList"
-import { IMovie } from "../../../../interface/IMovie"
+import React, { useCallback, useState, useEffect } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import {
+  makeStyles,
+  createStyles,
+  useTheme,
+  Theme,
+} from "@material-ui/core/styles";
+import { SearchBox } from "../../../Main/SearhMain/components/SearchBox";
+import { green, red } from "@material-ui/core/colors";
+import Icon from "@material-ui/core/Icon";
+import { useStyles } from "./styles";
+import MovieList from "./SearchMovieList/SearchMovieList";
+import { IMovie } from "../../../../interface/IMovie";
+import { cpuUsage } from "process";
 
 interface props {
-  open: boolean
-  callback: Function
-  totalImage: IMovie[]
+  open: boolean;
+  callback: Function;
+  totalImage: IMovie[];
 }
-export const ResponsiveDialog: React.FunctionComponent<props> = ({ open, callback, totalImage }) => {
-  const theme = useTheme()
-  const fullScreen = useMediaQuery(theme.breakpoints.down("xs"))
+export const ResponsiveDialog: React.FunctionComponent<props> = ({
+  open,
+  callback,
+  totalImage,
+}) => {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("xs"));
 
-  let [movies, setMovies] = useState<IMovie[]>([])
+  let [movies, setMovies] = useState<IMovie[]>([]);
 
   useEffect(() => {
-    setMovies(totalImage)
-  }, [totalImage])
+    setMovies(totalImage);
+  }, [totalImage]);
 
-  const classes = useStyles()
+  const classes = useStyles();
 
   const clickOk = () => {
     // setOpen(true)
-    callback(movies)
-  }
+    callback(movies);
+  };
 
   const handleClose = () => {
-    callback()
-    setTimeout(() => {
-      setMovies(totalImage)
-    }, 200)
-  }
+    callback();
+    // setTimeout(() => {
+    //   setMovies(totalImage);
+    // }, 200);
+  };
 
   const removeMovie = (e, i) => {
-    e.stopPropagation()
+    e.stopPropagation();
 
-    movies = movies.filter((iter, index) => i !== index)
-    setMovies(movies)
-  }
+    movies = movies.filter((iter, index) => i !== index);
+    setMovies(movies);
+  };
 
   const setSearchMovieCallback = (param) => {
     // movies = movies.unshift(param);
-    setMovies([param, ...movies])
-  }
+    setMovies([param, ...movies]);
+  };
   // const setSearchMovieCallback = useCallback((param) => {
   //   console.log(movies);
   //   // setMovies([...param, ...movies]);
@@ -66,11 +76,23 @@ export const ResponsiveDialog: React.FunctionComponent<props> = ({ open, callbac
 
   return (
     <div>
-      <Dialog style={{ top: "20%", maxHeight: "500px" }} fullScreen={fullScreen} open={open} onClose={handleClose} aria-labelledby="responsive-dialog-title">
-        <DialogTitle id="responsive-dialog-title">{"Use Google's location service?"}</DialogTitle>
+      <Dialog
+        style={{ top: "20%", maxHeight: "500px" }}
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
 
         <DialogContent>
-          <SearchBox styleFlag={"dialog"} callback={setSearchMovieCallback} text={"원하는 영화를 검색해주세요"} />
+          <SearchBox
+            styleFlag={"dialog"}
+            callback={setSearchMovieCallback}
+            text={"원하는 영화를 검색해주세요"}
+          />
 
           {/* <DialogContentText> */}
 
@@ -88,6 +110,6 @@ export const ResponsiveDialog: React.FunctionComponent<props> = ({ open, callbac
         </DialogActions>
       </Dialog>
     </div>
-  )
-}
-export default ResponsiveDialog
+  );
+};
+export default ResponsiveDialog;
